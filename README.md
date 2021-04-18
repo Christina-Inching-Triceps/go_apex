4b60aebd1603061ffeb566a42213a6d3a34bcc29
Christina-Inching-Triceps <christina.inching.triceps@gmail.com>
2021-04-19 03:56:43 +0900

    add apex tracker gg api call

---
+++ README.md
@@ -0,0 +1,7 @@
+# Gopex
+
+apex.tracker.gg API sample
+
+## Note
+
+- use validate lib (ex: https://github.com/go-playground/validator )
--- server.go
+++ server.go
@@ -1,26 +1,13 @@
 package main
 
 import (
-	"fmt"
-	"gopex/config"
 	"gopex/routes"
-
 	"github.com/labstack/echo/v4"
 )
 
 func main() {
 	// ===== Init Echo Server
 	e := echo.New()
-
-	// ===== Load Config
-	 config, err := config.Load()
-	 if err != nil {
-		 e.Logger.Fatal(err)
-	 }
-
-	 // TODO
-	 fmt.Printf("[Debug] token: %s \n", config.Apex.Token)
-	 _ = config
 
 	// ===== Setup Router
 	routes.Router(e)
